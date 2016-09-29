cdb616f1cabd67269ee9100505f4ed7e1f422e26
Nathan Beck <njosefbeck@gmail.com>
2016-09-29 16:11:15 -0500

    Add js transform gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
+import babel from 'gulp-babel';
 
 const dirs = {
 	app: 'app',
@@ -12,11 +13,21 @@ const sassPaths = {
 	src: `${dirs.app}/scss/style.scss`
 };
 
+const jsPaths = {
+	allFiles: `${dirs.app}/js/**/*.js` 
+}
+
 gulp.task('styles', () => {
 	return gulp.src(sassPaths.src)
 		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(autoprefixer())
 		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(dirs.app));
+});
+
+gulp.task('js', () => {
+	return gulp.src(jsPaths.allFiles)
+		.pipe(babel({ presets: ['es2015'] }))
 		.pipe(gulp.dest(dirs.app));
 });
