daea0a2440989048e849614e01f94a8d12929d21
Nathan Beck <njosefbeck@gmail.com>
2016-11-02 14:58:24 -0500

    Refine everything further

--- app/js/main.js
+++ app/js/main.js
@@ -6,23 +6,28 @@ jQuery(document).ready(() => {
 	'use strict';
 
 	var centeredTile = {};
-	//var tileWidth = document.documentElement.clientWidth - 20;
-	let tileWidth = 640;
-	//var tileHeight = document.documentElement.clientHeight;
-	let tileHeight = 480;
+	var documentWidth = document.documentElement.clientWidth;
+	var documentHeight = document.documentElement.clientHeight;
+
+	let tileWidth = 750;
+	let tileHeight = 495;
+
+	if (documentWidth <= 750) {
+		tileWidth = documentWidth - 10;
+		tileHeight = ( 495 / 750 ) * tileWidth;
+	}
+	
 	let count = 1;
 	let numberOfImages = 55;
-	const wall = jQuery.infinitedrag('#wall', {}, {
+	const wall = jQuery.infinitedrag('#wall', { elementsWithInteraction: '.info, .get-info'}, {
 		width: tileWidth,
 		height: tileHeight,
-		//range_col: [0,10],
-		//range_row: [0, 10],
 		oncreate: function($element, col, row) {
 			if(count > numberOfImages) {
 				count = 1;
 			}
 
-			if (count === 10) {
+			if (count === 1) {
 				centeredTile.element = $element;
 				centeredTile.col = col;
 				centeredTile.row = row;
@@ -33,12 +38,12 @@ jQuery(document).ready(() => {
 		}
 	});
 
-	centeredTile.element.append('<div class="content-container">davey rocco</div>')
-	centeredTile.element.append('<div class="info">Information<div class="close-info">X</div></div>');
+	centeredTile.element.append('<div class="content-container"><div class="content"><h1>davey rocco</h1><p class="get-info">get info</p></div></div>');
+	centeredTile.element.append('<div class="info"><div class="close-info">X</div><h2>Information</h2><p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus.</p><p>Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, <a href="#">a pharetra augue. Fusce dapibus,</a> tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui.</p><p>Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui.</p><p>Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui.</p><p>Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui.</p><p>Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui.</p></div>');
 
 	wall.center(centeredTile.col, centeredTile.row);
 
-	jQuery('.content-container').on('click', showInfo);
+	jQuery('.get-info').on('click', showInfo);
 	jQuery('.close-info').on('click', hideInfo);
 	jQuery('.home').on('click', centerTiles);
 
@@ -46,7 +51,7 @@ jQuery(document).ready(() => {
 		jQuery('.info').addClass('show');
 	}
 
-	function hideInfo() {
+	function hideInfo(e) {
 		jQuery('.info').removeClass('show');
 	}
 
